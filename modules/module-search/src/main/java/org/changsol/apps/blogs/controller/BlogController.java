bb67a3a4a10c8eb779887d4c8a1a229f6bccd3c3
ChangSol <ckdwnsla12@naver.com>
2023-04-02 20:09:12 +0900

    fix search validated remove

--- modules/module-search/src/main/java/org/changsol/apps/blogs/controller/BlogController.java
+++ modules/module-search/src/main/java/org/changsol/apps/blogs/controller/BlogController.java
@@ -10,14 +10,12 @@ import org.changsol.apps.blogs.dto.BlogDto;
 import org.changsol.apps.blogs.service.BlogService;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "블로그 검색 서비스", description = "블로그 검색 서비스입니다.") // Swagger API 명 설정
-@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "/v1/blogs")
